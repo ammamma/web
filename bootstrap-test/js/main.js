cb44da6c29f197c47d3a24c07429812f61c65ca5
ammamma <marko.p.makinen@gmail.com>
2016-05-04 16:04:38 +0300

    Bootstrap test page sections, nicescroll scrollbar and smooth scroll.

--- bootstrap-test/js/main.js
+++ bootstrap-test/js/main.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
   //alert("Test alert from main.js!");
+  $("html").niceScroll();
+
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
+  
 });
